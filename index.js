644b7b0c0a02aa2ac652ca89549088d380dba854
TyreseMugoNyaga <tymugo@gmail.com>
2023-10-09 23:03:22 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+
+function upperCaseCustomerName (){
+    customerName = customerName.toUpperCase();
+
+
+}
+
+function setBestCustomer (){
+
+    bestCustomer = 'not bob' ;
+
+}
+function overwriteBestCustomer (){
+    bestCustomer = 'maybe bob'
+}
+
+const leastfavoritecustomer = 'dan'
+
+function changeLeastFavoriteCustomer(){
+ const leastfavoritecustomer2 = leastfavoritecustomer;
+ leastfavoritecustomer2 = "cate"
+}
